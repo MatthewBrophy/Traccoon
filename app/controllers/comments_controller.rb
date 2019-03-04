546ec7fde61508534b149dff399d0730ef341fa9
Tiffany <Tafby88@gmail.com>
2019-03-04 11:39:47 -0800

    added show, index, new info

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,24 @@
 class CommentsController < ApplicationController
+
+  def index
+  @comments = Comment.all
+  end
+  
+  def show
+    @comment = Comment.find(params[:id])
+  end
+
+  def new
+    @comment = Comment.new
+  end 
+
+  def create
+    
+  end
+
+# private
+#   def comment_params
+#     params.require(:comments).permit()
+#   end
+
 end
--- app/controllers/raccoons_controller.rb
+++ app/controllers/raccoons_controller.rb
@@ -1,2 +1,24 @@
 class RaccoonsController < ApplicationController
+  def index
+    @raccoons = Raccoon.all
+  end
+
+  def show 
+    @raccoon = Raccoon.find(params[:id])
+  end
+
+  def new
+    @raccoon = Raccoon.new
+  end
+
+  def create
+    @raccoon = Raccoon.create(raccoon_params)
+    redirect_to raccoon_path(@raccoon)
+  end
+
+  private 
+
+  def raccoon_params
+    params.require(:raccoons).permit(:name, :color, :size, :gender, :favorite_food, :disposition)
+  end
 end
