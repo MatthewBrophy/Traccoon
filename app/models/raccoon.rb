6f10f775d15ff6738e3a239f13b4931bfda85c67
Matthew Brophy <MBrophy@Matthews-MacBook-Air.local>
2019-03-06 17:43:52 -0800

    working on tracking page and tracking methods

--- app/models/raccoon.rb
+++ app/models/raccoon.rb
@@ -12,13 +12,25 @@ class Raccoon < ApplicationRecord
     neighborhood_id = neighborhoods_visited.ids.max_by { |i| neighborhoods_visited.count(i) }
     Neighborhood.find(neighborhood_id).name
   end
-
+  
+  
   def sightings
     sightings_collection = []
     self.locations.each {|i| sightings_collection << i.address}
     sightings_collection
   end
-
+  
+  def gmaps_sightings
+    sightings_collection = []
+    self.locations.each {|i| sightings_collection << i.address}
+    sightings_collection.map {|address| 
+      breakdown = address.split(",")
+      number_and_street = breakdown[0].gsub(" ","+")
+      breakdown.shift
+      city_and_state = breakdown.map {|i| i.strip}.join("+")
+      "#{number_and_street},#{city_and_state}"}
+  end
+  
   def sizes
     ["small", "medium", "large", "massive"]
   end
