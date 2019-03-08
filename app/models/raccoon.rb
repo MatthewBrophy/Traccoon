89837120993fed9c36ff3b897c48d4c1f46a32dd
Matthew Brophy <MBrophy@Matthews-MacBook-Air.local>
2019-03-08 11:02:11 -0800

    cute raccoon icon

--- app/models/raccoon.rb
+++ app/models/raccoon.rb
@@ -30,6 +30,8 @@ class Raccoon < ApplicationRecord
       city_and_state = breakdown.map {|i| i.strip}.join("+")
       "#{number_and_street},#{city_and_state}"}
   end
+
+  
   
   def sizes
     ["small", "medium", "large", "massive"]
@@ -41,9 +43,5 @@ class Raccoon < ApplicationRecord
 
   def dispositions
     ["friendly", "shy", "aggressive", "loving", "defensive"]
-  end
-
-  def pictures
-    ["raccoon1.jpg", "raccoon2.jpg", "raccoon3.jpg", "raccoon4.jpg", "raccoon5.jpg", "raccoon6.jpg", "raccoon7.jpg", "raccoon8.jpg", "raccoon9.jpg", "raccoon10.jpg", "raccoon11.jpg", "raccoon12.jpg", "raccoon13.jpg", "raccoon14.jpg", "raccoon15.jpg", "raccoon16.jpg", "raccoon17.jpg", "raccoon18.jpg", "raccoon19.jpg", "raccoon20.jpg", "raccoon21.jpg", "raccoon22.jpg", "raccoon23.jpg", "raccoon24.jpg", "raccoon25.jpg", "raccoon26.jpg", "raccoon27.jpg", "raccoon28.jpg", "raccoon29.jpg", "raccoon30.jpg", "raccoon31.jpg"]
   end
 end
